ec43f124dacaf26a16dacc74e33a144a85776cc5
Riswanto <riswanto.aris@gmail.com>
2021-07-16 16:33:21 +0700

    3rd commit

--- laporankasus/views.py
+++ laporankasus/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, HttpResponse
 def home_dashboard(request):
     group = request.user.groups.values_list('name', flat=True).first()
     if group == 'superadmin':
-        return HttpResponse('ini adalah Halaman super admin')
+        # return HttpResponse('ini adalah Halaman super admin')
+        context = {}
+        return render(request, 'laporankasus/halaman_superadmin.html', context)
     elif group == 'admin':
         return HttpResponse('ini adalah halaman admin pusat')
     elif group == 'adminsatker':
